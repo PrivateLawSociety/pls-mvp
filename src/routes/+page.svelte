532dd1f7390c672218136fab433a775f82afa0b6
marcusmmm <marcusizabel230@gmail.com>
2023-09-20 14:24:48 -0300

    feat: home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen w-full gap-4">
+	<Button>
+		<a href="/multisig/generate">Generate multisig</a>
+	</Button>
+	<Button>
+		<a href="/multisig/start">Start multisig spend</a>
+	</Button>
+	<Button>
+		<a href="/multisig/finish">Finish multisig spend</a>
+	</Button>
+</div>
